07a70f9700b505d6766e572b8f655087cd747a07
Parker Tenove <parkertenover@gmail.com>
2018-11-26 11:53:28 -0800

    Yo

--- app.js
+++ app.js
@@ -4,42 +4,42 @@ const methodOverride = require('method-override')
 var exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/contractor-project');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/u-glass');
 // Here we go
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(methodOverride('_method'))
 
-// Make an Issue
-app.post('/issues', (req, res) => {
-  issue.create(req.body).then((issue) => {
-    console.log(issue)
-    res.redirect(`/issues/${issue._id}`)
+// Make a product
+app.post('/products', (req, res) => {
+  product.create(req.body).then((product) => {
+    console.log(product)
+    res.redirect(`/products/${product._id}`)
   }).catch((err) => {
     console.log(err.message)
   })
 })
-// Change an issue you have created
-app.put('/issues/:id', (req, res) => {
-  issue.findByIdAndUpdate(req.params.id, req.body)
-    .then(issue => {
-      res.redirect(`/issues/${issue._id}`)
+// Change an product you have created
+app.put('/products/:id', (req, res) => {
+  product.findByIdAndUpdate(req.params.id, req.body)
+    .then(product => {
+      res.redirect(`/products/${product._id}`)
     })
     .catch(err => {
       console.log(err.message)
     })
 })
-// Delete an issue
-app.delete('/issues/:id', function (req, res) {
-  console.log("DELETE issue")
-  issue.findByIdAndRemove(req.params.id).then((issue) => {
+// Delete an product
+app.delete('/products/:id', function (req, res) {
+  console.log("DELETE product")
+  product.findByIdAndRemove(req.params.id).then((product) => {
     res.redirect('/');
   }).catch((err) => {
     console.log(err.message);
   })
 })
-
-const issue = mongoose.model('issue', {
+// Model
+const product = mongoose.model('product', {
   title: String,
   description: String,
   movieTitle: String,
@@ -52,30 +52,34 @@ app.set('view engine', 'handlebars');
 
 // Index Page
 app.get('/', (req, res) => {
-  issue.find()
-    .then(issues => {
-      res.render('issues-index', { issues: issues });
+  product.find()
+    .then(products => {
+      res.render('products-index', { products: products });
     })
     .catch(err => {
       console.log(err);
     })
 })
-// New Issue
-app.get('/issues/new', (req, res) => {
-  res.render('issues-new', {});
+// New product
+app.get('/products/new', (req, res) => {
+  res.render('products-new', {});
 })
-// Show Issue
-app.get('/issues/:id', (req, res) => {
-  issue.findById(req.params.id).then((issue) => {
-    res.render('issues-show', { issue: issue })
+// About
+app.get('/products/about', (req, res) => {
+  res.render('products-about', {});
+})
+// Show product
+app.get('/products/:id', (req, res) => {
+  product.findById(req.params.id).then((product) => {
+    res.render('products-show', { product: product })
   }).catch((err) => {
     console.log(err.message);
   })
 })
-// Edit Issue
-app.get('/issues/:id/edit', (req, res) => {
-  issue.findById(req.params.id, function(err, issue) {
-    res.render('issues-edit', {issue: issue});
+// Edit product
+app.get('/products/:id/edit', (req, res) => {
+  product.findById(req.params.id, function(err, product) {
+    res.render('products-edit', {product: product});
   })
 })
 app.listen(process.env.PORT || 3000, () => {
